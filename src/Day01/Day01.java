e090dd84c5941cb80115ea4f7682e3780b7723b9
dkacperzyk <dawid.kacperzyk@infor.com>
2024-12-05 10:13:50 +0100

    Day01 - Part 1

---
+++ src/Day01/Day01.java
@@ -0,0 +1,37 @@
+package Day01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Day01 {
+
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<>(), l2 = new ArrayList<>();
+        readInput(l1, l2);
+        l1.sort(Comparator.comparing(Integer::intValue));
+        l2.sort(Comparator.comparing(Integer::intValue));
+        System.out.println(IntStream.range(0, l1.size()).parallel().map(i -> Math.abs(l1.get(i) - l2.get(i))).sum());
+    }
+
+    private static void readInput(List<Integer> l1, List<Integer> l2) {
+        try (BufferedReader br = new BufferedReader(new FileReader(Day01.class.getResource("input.txt").getPath()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] columns = line.split("\\s+");
+                if (columns.length == 2) {
+                    l1.add(Integer.valueOf(columns[0]));
+                    l2.add(Integer.valueOf(columns[1]));
+                } else {
+                    throw new Exception("Invalid format. Expecting two columns for each line.");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
